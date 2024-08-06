200e96eca0a10298c8aa6f482eb001faea623365
Aryanmandal2 <159801631+Aryanmandal8670@users.noreply.github.com>
2024-08-07 00:58:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,29 +3,29 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://chat.whatsapp.com/EXAbTLuZgU2C1n18VtSwUZ'
+global.linkGroup = 'https://chat.whatsapp.com/EXAbTLuZgU2C1n18VtSwUZ'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
-global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
-global.sessionName = 'session';
-global.prefa = [".", "!", "/", "?"];
+global.ownername = "Aryan Sir";
+global.owner = ['918670755006'];
+global.premium = ['919775541336'];
+global.packname = "aryan op";
+global.author = "ARYAN OP SIR";
+global.sessionName = 'Aryan op';
+global.prefa = "."
 global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
 global.autoread = false
 global.available = false
-global.autoTyping = false
-global.autoRecord = false
+global.autoTyping = true
+global.autoRecord = true
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
-global.welcome = true
-global.autoBlock = true
+global.welcome = false
+global.autoBlock = false
  
 /*REPLY MESSAGE*/
 global.mess = {
